7c7e8ca5498d0ebd195452fa650f4f7f8831f835
Gabriel Abubakar <gabrielogrima@gmail.com>
2021-10-13 10:32:44 +0100

    project item configuration

--- src/components/projectItem/projectItem.js
+++ src/components/projectItem/projectItem.js
@@ -7,13 +7,15 @@ const ProjectItem = (props) => {
     return(
 
         <div className="project__item">
-            <a href={props.link} target="_blank" rel="noreferrer">
-                <figure>
-                    <img src={props.image} alt={props.alt} width="1080" height="561"/>
-                </figure>
-                <h3>{props.title}</h3>
-                <p>{props.details}</p>
-            </a>
+                <a href={props.link} target="_blank" rel="noreferrer">
+                    <figure>
+                        <img src={props.image} alt={props.alt} width="1080" height="561"/>
+                    </figure>
+                </a>
+                <div>
+                    <h3>{props.title}</h3>
+                    <p>{props.details}</p>
+                </div>
         </div>
     )
 }
