3ca69e0f00756928413a53748f1dcab5f9de66f0
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-03-22 01:31:27 +0100

    react reveal

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -1,34 +1,47 @@
 import React from "react";
 import "./about.scss"
 import Heading from "../../components/heading/heading";
+import Fade from 'react-reveal/Fade';
 
 const About = () => {
-    
+
     return (
 
         <section className="about">
-            
-            <Heading title="About." name="about"/>
+
+            <Heading title="About." name="about" />
 
             <article className="about__text-container">
-                <p className="about__text">
-                    Hi there, I’m Gabriel, a self taught front-end web developer currently surviving Lagos, Nigeria.
-                </p>
-                <p className="about__text">
-                    I’m interested in creating user web experiences out of designs by writing as little and efficient code as possible so as to reduce load times thereby creating awesome site experiences.🚀🚀
-                </p>
-                <p className="about__text">
-                    I have been writing frontend code since 2020 and over time have acquired various skills that have advanced my development workflow.
-                </p>
-                <p className="about__text">
-                    These skillls include competency in the building blocks of the web (HTML, CSS and Javascript), alongside other frontend frameworks and libraries such as React.js, Vue.js and Next.js.
-                </p>
-                <p className="about__text">
-                    I have also worked personally with further concepts such as Web Accessibility, Site Optimization for Search Engines as well as Prototyping wireframes via products such as Figma and Adobe XD.
-                </p>
-                <p className="about__text">
-                    I’m open to learning new web technologies in other to be able to work efficiently in diverse development teams.🙃 
-                </p>
+                <Fade bottom>
+                    <p className="about__text">
+                        Hi there, I’m Gabriel, a self taught front-end web developer currently surviving Lagos, Nigeria.
+                    </p>
+                </Fade>
+                <Fade bottom>
+                    <p className="about__text">
+                        I’m interested in creating user web experiences out of designs by writing as little and efficient code as possible so as to reduce load times thereby creating awesome site experiences.🚀🚀
+                    </p>
+                </Fade>
+                <Fade bottom>
+                    <p className="about__text">
+                        I have been writing frontend code since 2020 and over time have acquired various skills that have advanced my development workflow.
+                    </p>
+                </Fade>
+                <Fade bottom>
+                    <p className="about__text">
+                        These skillls include competency in the building blocks of the web (HTML, CSS and Javascript), alongside other frontend frameworks and libraries such as React.js, Vue.js and Next.js.
+                    </p>
+                </Fade>
+                <Fade bottom>
+                    <p className="about__text">
+                        I have also worked personally with further concepts such as Web Accessibility, Site Optimization for Search Engines as well as Prototyping wireframes via products such as Figma and Adobe XD.
+                    </p>
+                </Fade>
+                <Fade bottom>
+                    <p className="about__text">
+                        I’m open to learning new web technologies in other to be able to work efficiently in diverse development teams.🙃
+                    </p>
+                </Fade>
 
                 <a href="www.googledrive.com">Resume</a>
             </article>
--- src/sections/mainSection/mainSection.js
+++ src/sections/mainSection/mainSection.js
@@ -1,19 +1,23 @@
 import React from "react"
 import Navbar from "../../components/navbar/navbar"
 import "./mainSection.scss"
+import Fade from 'react-reveal/Fade';
+
 
 const MainSection = () => {
 
     return (
 
         <section className="mainSection">
-            
+
             <Navbar />
 
-            <h1>
-                <span className="brown">Hi there🖐🏼.</span> I am <span className="whiteBold">Gabriel, </span><br />
-                a <span className="white">frontend developer</span> based in <span className="brown">Lagos, Nigeria</span> dedicated to creating awesome and accesible web experiences.  
-            </h1>
+            <Fade bottom duration={400}>
+                <h1>
+                    <span className="brown">Hi there🖐🏼.</span> I am <span className="whiteBold">Gabriel, </span><br />
+                    a <span className="white">frontend developer</span> based in <span className="brown">Lagos, Nigeria</span> dedicated to creating awesome and accesible web experiences.
+                </h1>
+            </Fade>
         </section>
     )
 }
